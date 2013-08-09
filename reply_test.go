167f0908f1dae1ce019796cf22d38b7308dace1e
Guillaume J. Charmes <guillaume.charmes@dotcloud.com>
2013-08-09 11:56:15 -0700

    Fix issue within test

--- reply_test.go
+++ reply_test.go
@@ -15,9 +15,9 @@ func TestWriteStatus(t *testing.T) {
 		{&ErrorReply{code: "ERROR", message: "Something went wrong"}, "-ERROR Something went wrong\r\n"},
 		{&BulkReply{}, "$-1\r\n"},
 		{&BulkReply{[]byte{'h', 'e', 'l', 'l', 'o'}}, "$5\r\nhello\r\n"},
-		{&MultiBulkReply{[][]byte{[]byte{'h', 'e', 'l', 'l', 'o'}}}, "*1\r\n$5\r\nhello\r\n"},
-		{&MultiBulkReply{[][]byte{[]byte{'h', 'e', 'l', 'l', 'o'}, []byte{'h', 'i'}}}, "*2\r\n$5\r\nhello\r\n$2\r\nhi\r\n"},
-		{&MultiBulkReply{[][]byte{nil, []byte{'h', 'e', 'l', 'l', 'o'}, nil, []byte{'h', 'i'}}}, "*4\r\n$-1\r\n$5\r\nhello\r\n$-1\r\n$2\r\nhi\r\n"},
+		{&MultiBulkReply{[][]byte{{'h', 'e', 'l', 'l', 'o'}}}, "*1\r\n$5\r\nhello\r\n"},
+		{&MultiBulkReply{[][]byte{{'h', 'e', 'l', 'l', 'o'}, {'h', 'i'}}}, "*2\r\n$5\r\nhello\r\n$2\r\nhi\r\n"},
+		{&MultiBulkReply{[][]byte{nil, {'h', 'e', 'l', 'l', 'o'}, nil, {'h', 'i'}}}, "*4\r\n$-1\r\n$5\r\nhello\r\n$-1\r\n$2\r\nhi\r\n"},
 		{MultiBulkFromMap(&map[string][]byte{"hello": []byte("there"), "how": []byte("are you")}), "*4\r\n$5\r\nhello\r\n$5\r\nthere\r\n$3\r\nhow\r\n$7\r\nare you\r\n"},
 	}
 	for _, p := range replies {
@@ -30,7 +30,7 @@ func TestWriteStatus(t *testing.T) {
 		if val != p.expected {
 			t.Fatalf("Oops, expected %q, got %q instead", p.expected, val)
 		}
-		if n != len(p.expected) {
+		if n != int64(len(p.expected)) {
 			t.Fatalf("Expected to write %d bytes, wrote %d instead", len(p.expected), n)
 		}
 	}
