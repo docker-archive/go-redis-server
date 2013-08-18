5ddf4e58c1f79f09e9b27162658e335a833da924
Solomon Hykes <solomon@dotcloud.com>
2013-08-17 19:06:48 -0700

    Add package description for go doc.

--- server.go
+++ server.go
@@ -1,3 +1,7 @@
+// go-redis-server is a helper library for building server software capable of speaking the redis protocol.
+// This could be an alternate implementation of redis, a custom proxy to redis,
+// or even a completely different backend capable of "masquerading" its API as a redis database.
+
 package redis
 
 import (
