d844598dfe922b79118e78936d44dff239f90d22
Guillaume J. Charmes <guillaume.charmes@dotcloud.com>
2013-08-19 15:16:30 -0700

    Implement a RegisterFct in order to map non-object function into redis commands

--- example/main.go
+++ example/main.go
@@ -9,12 +9,12 @@ type MyHandler struct {
 	redis.DefaultHandler
 }
 
-// TEST implement a new command. Non-redis standard, but it is possible.
+// Test implement a new command. Non-redis standard, but it is possible.
 func (h *MyHandler) Test() ([]byte, error) {
 	return []byte("Awesome custom redis command!"), nil
 }
 
-// GET override the DefaultHandler's method.
+// Get override the DefaultHandler's method.
 func (h *MyHandler) Get(key string) ([]byte, error) {
 	// However, we still can call the DefaultHandler GET method and use it.
 	ret, err := h.DefaultHandler.GET(key)
@@ -24,6 +24,12 @@ func (h *MyHandler) Get(key string) ([]byte, error) {
 	return []byte("BEAM/" + string(ret)), err
 }
 
+// Test2 implement a new command. Non-redis standard, but it is possible.
+// This function needs to be registered.
+func Test2() ([]byte, error) {
+	return []byte("Awesome custom redis command via function!"), nil
+}
+
 func main() {
 	defer func() {
 		if msg := recover(); msg != nil {
@@ -31,8 +37,12 @@ func main() {
 		}
 	}()
 
-	handler, err := redis.NewAutoHandler(&MyHandler{})
+	myhandler := &MyHandler{}
+	handler, err := redis.NewAutoHandler(myhandler)
 	if err != nil {
+		panic(err)
+	}
+	if err := handler.RegisterFct("test2", Test2); err != nil {
 		panic(err)
 	}
 	server := &redis.Server{
