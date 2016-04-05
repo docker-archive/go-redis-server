aa33f76823c965fb06e004df57b3a188bb21455e
Tom Grennan <tgrennan@platinasystems.com>
2016-04-05 11:33:30 -0700

    optimize empty DEBUG with a closure
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- debug.go
+++ debug.go
@@ -10,21 +10,28 @@ import (
 
 var Stderr = io.Writer(os.Stderr)
 
-// Debug function, if the debug flag is set, then display. Do nothing otherwise
+// This closure is a no-op unless the DEBUG env is non empty.
+var Debugf = func(format string, a ...interface{}) {}
+
+func init() {
+	if os.Getenv("DEBUG") != "" {
+		Debugf = ActualDebugf
+	}
+
+}
+
 // If Docker is in damon mode, also send the debug info on the socket
 // Convenience debug function, courtesy of http://github.com/dotcloud/docker
-func Debugf(format string, a ...interface{}) {
-	if os.Getenv("DEBUG") != "" {
-
-		// Retrieve the stack infos
-		_, file, line, ok := runtime.Caller(1)
-		if !ok {
-			file = "<unknown>"
-			line = -1
-		} else {
-			file = file[strings.LastIndex(file, "/")+1:]
-		}
-
-		fmt.Fprintf(Stderr, fmt.Sprintf("[%d] [debug] %s:%d %s\n", os.Getpid(), file, line, format), a...)
+func ActualDebugf(format string, a ...interface{}) {
+	// Retrieve the stack infos
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+		file = "<unknown>"
+		line = -1
+	} else {
+		file = file[strings.LastIndex(file, "/")+1:]
 	}
+	fmt.Fprintf(Stderr, "[%d] [debug] %s:%d ", os.Getpid(), file, line)
+	fmt.Fprintf(Stderr, format, a...)
+	fmt.Fprintln(Stderr)
 }
